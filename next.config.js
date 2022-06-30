ceefe3d3b29be3965a9b6ab238a8ccce0fd4e62e
Ramazan USLU <ruslu1720@gmail.com>
2022-06-30 22:20:01 +0300

    redirects eklendi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/optimizasyon/image",
+        destination: "/optimizasyon",
+        permanent: false,
+      },
+    ];
+  },
+};
